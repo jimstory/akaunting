47a3d09b3e6a10a28aa6ce0ed57c23288762eb5b
jimstory <>
2018-09-11 10:49:34 +0800

    link changed

--- resources/views/partials/admin/header.blade.php
+++ resources/views/partials/admin/header.blade.php
@@ -7,12 +7,12 @@
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><img src="{{ asset('public/img/akaunting-logo-white.png') }}" class="logo-image-mini" width="25" alt="Akaunting Logo"></span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><img src="{{ asset('public/img/akaunting-logo-white.png') }}" class="logo-image-lg" width="25" alt="Akaunting Logo"> <b>Akaunting</b></span>
+            <span class="logo-lg"><img src="{{ asset('public/img/akaunting-logo-white.png') }}" class="logo-image-lg" width="25" alt="Akaunting Logo"> <b>MSCFO</b></span>
         @else
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><img src="{{ asset('public/img/akaunting-logo-green.png') }}" class="logo-image-mini" width="25" alt="Akaunting Logo"></span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><img src="{{ asset('public/img/akaunting-logo-green.png') }}" class="logo-image-lg" width="25" alt="Akaunting Logo"> <b>Akaunting</b></span>
+            <span class="logo-lg"><img src="{{ asset('public/img/akaunting-logo-green.png') }}" class="logo-image-lg" width="25" alt="Akaunting Logo"> <b>MSCFO</b></span>
         @endif
     </a>
     <!-- Header Navbar: style can be found in header.less -->
@@ -159,7 +159,7 @@
                 @endpermission
                 <!-- Updates: style can be found in dropdown.less -->
                 <li class="hidden-xs">
-                    <a href="{{ url('https://akaunting.com/docs') }}" target="_blank" title="{{ trans('general.help') }}">
+                    <a href="{{ url('https://baidu.com/docs') }}" target="_blank" title="{{ trans('general.help') }}">
                         <i class="fa fa-life-ring"></i>
                     </a>
                 </li>
