b13c71a948d8c24baf05a49df340b96f7e33e107
jimstory <>
2018-09-11 15:25:30 +0800

    modules language zh-CN

---
+++ modules/OfflinePayment/Resources/lang/zh-CN/offlinepayment.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'offlinepayment'   => '线下支付',
+    'add_new'          => '新增',
+    'edit'             => '编辑: :method',
+    'code'             => 'Code',
+    'customer'         => '客户可见',
+    'order'            => '排序',
+    'payment_gateways' => '线下支付方式',
+
+    'confirm'          => '确认',
+    'loading'          => 'Loading',
+
+];
---
+++ modules/PaypalStandard/Resources/lang/zh-CN/paypalstandard.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'paypalstandard'   => 'Paypal',
+    'email'            => 'Email',
+    'mode'             => '网关模式',
+    'debug'            => 'Debug',
+    'transaction'      => 'Transaction',
+    'customer'         => '客户可见',
+    'order'            => '排序',
+
+    'test_mode'        => '警告: 支付网关处于“沙箱模式”。 我们不会向您的帐户扣款。',
+    'description'      => '用PAYPAL付款',
+    'confirm'          => '确认',
+
+];
